0edbf9670543e841b8bf5e3e28d691d0cdcd4f30
Anthony HORNEC <anthony.hornec@outlook.fr>
2025-12-22 17:38:09 +0100

    feat: add Matomo for tracking

--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import VueMatomo from 'vue-matomo'
 
 createApp(App)
+  .use(VueMatomo, {
+    host: 'https://analytics.informagic-landes.fr',
+    siteId: 1,
+    router: router,
+    requireConsent: true,
+    requireCookieConsent: true,
+    trackInitialView: true,
+  })
   .use(router)
   .mount('#app')
+
+window._paq.push(['trackPageView'])
